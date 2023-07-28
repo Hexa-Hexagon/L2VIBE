c9b8ee82fc74a991c8167f90cde2cefc6af40237
Ehor <ehorlukianov@gmail.com>
2023-07-28 12:41:09 +0300

    Add config

---
+++ config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    PORT: process.env.PORT || 5000,
+    DB: {
+        DB_HOST: "127.0.0.1",
+        DB_NAME: "l2vibe.com",
+        DB_PORT: 27017
+    }
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./app');
+const { PORT } = require('./config/index');
 
 const server = http.createServer(app);
 
