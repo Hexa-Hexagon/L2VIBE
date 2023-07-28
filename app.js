87f3888ca8befd41efbe7927038686440fe4fce7
Ehor <ehorlukianov@gmail.com>
2023-07-28 18:14:29 +0300

    Add get request

--- app.js
+++ app.js
@@ -4,4 +4,14 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', async(req, res) => {
+    try {
+        const sites = MODEL.site.find();
+        res.json(sites);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+
 module.exports = app;
