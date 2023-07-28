fc4a3c24f9598bbf593cbabfeaeb0b57744b51c5
Ehor <ehorlukianov@gmail.com>
2023-07-28 12:45:23 +0300

    Add model

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const MODEL = require('./model/');
 const app = express();
 
 app.use(express.json());
---
+++ model/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { DB } = require('../config/index');
+const DB_URL = `mongodb://${DB.DB_HOST}:${DB.DB_PORT}/${DB.DB_PORT}`;
+
+async function connect() {
+    try {
+        await mongoose.connect(DB_URL);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connect();
+
+module.exports = {};
