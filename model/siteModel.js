e6c4a0c9988c122f59cb296278712b96a52544f3
Ehor <ehorlukianov@gmail.com>
2023-07-31 13:20:45 +0300

    Update site model

--- model/siteModel.js
+++ model/siteModel.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const siteSchema = new mongoose.Schema({
     nameSite: { type: String, required: true, min: 1, max: 64 },
     status: { type: String, required: true, min: 1, max: 8 },
+    difficulty: { type: Number, required: true },
+    version: { type: String, required: true },
     dateOfStartingServer: { type: Date, required: true },
     dateOfEndingContract: { type: Date, required: true, expires: 1 }
 });
